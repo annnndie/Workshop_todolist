855d108a94ce39584b3ce791c787f77db8918dde
lihsin <lihsin0427@gmail.com>
2021-12-03 12:29:10 +0800

    點擊任務 list 可以切換 checked class

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,11 @@
 // TO DO
+document.addEventListener("DOMContentLoaded", function(){
+  
+  // Task_1 點擊任務 list 可以切換 checked class
+  const container = document.querySelector('ul');
+
+  container.addEventListener("click", (e) => {
+    if (e.target.tagName === 'LI')
+    e.target.classList.add("checked")
+  }) 
+})
