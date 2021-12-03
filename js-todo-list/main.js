8227fbe1a374db1673fbc6ff7b4af50c5a8515ad
Erik <jjjay5223758@gmai.com>
2021-12-03 17:01:06 +0800

    add t3

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,9 +1,38 @@
 // checked
 document.addEventListener("DOMContentLoaded", () => {
-    const todo = document.querySelector('ul');
-    todo.addEventListener('click', (e) => {
-        if (e.target.tagName ='li') {
+    const checked = document.querySelector('ul');
+    checked.addEventListener('click', (e) => {
+        if (e.target.nodeName ==='LI') {
         e.target.classList.toggle('checked');
         }
     })
+    //remove
+    const remove = document.querySelector('ul');
+    remove.addEventListener("click", (e) => {
+        if (e.target.nodeName === "SPAN") {
+        e.target.parentNode.remove();
+        }
+    });
+    //add
+    const add = document.querySelector("#addBtn");
+    const ul = document.querySelector("ul");
+
+    add.addEventListener("click", e => {
+
+        // get the input values
+        let header = e.target.parentElement;
+        let todoText = header.children[1].value;
+        console.log(todoText)
+
+        //add in li
+        let text = document.createElement("li")
+        let close = document.createElement("span")
+        text.innerHTML = todoText;
+        close.innerText = 'x'
+        close.className = "close"
+        text.append(close);
+        ul.append(text);
+        ul.insertAdjacentElement("beforebegin", ul)
+    })
 })
+
