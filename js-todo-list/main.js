af9ee237f3a569669685e5584ffc06524c8f8757
Laichu <cozzichilli@gmail.com>
2021-12-03 13:52:35 +0800

    添加刪除功能

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,13 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   const checkMark = document.querySelectorAll("li");
+  const deleteMark = document.querySelectorAll(".close")
 
 
   checkMark.forEach( (checkMark) => {
     checkMark.addEventListener("click", (e) => {
       e.target.classList.toggle('checked')
     })
- 
+  })
+
+  deleteMark.forEach( (deleteMark) => {
+    deleteMark.addEventListener("click", (e) => {
+      e.target.parentNode.remove()
+    });
   })
 
 
