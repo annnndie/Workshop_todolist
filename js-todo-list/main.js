ad5c506c04226e21bf9d2ba257c3250af0d3a979
Laichu <cozzichilli@gmail.com>
2021-12-03 12:35:25 +0800

    新增toggle 'checked'

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,14 @@
-// TO DO
+document.addEventListener("DOMContentLoaded", () => {
+
+  const checkMark = document.querySelectorAll("li");
+
+
+  checkMark.forEach( (checkMark) => {
+    checkMark.addEventListener("click", (e) => {
+      e.target.classList.toggle('checked')
+    })
+ 
+  })
+
+
+});
