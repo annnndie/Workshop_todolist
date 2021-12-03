aea5424ba7f07811b9486f12fd23ec93aa051005
patty <pattycheng2008@gmail.com>
2021-12-03 11:58:33 +0800

    add click close function

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,27 @@
 // TO DO
+document.addEventListener("DOMContentLoaded", () => {
+  const addBtn = document.querySelector("#addBtn");
+  const text = document.querySelector("#input");
+  const container = document.querySelector("#container");
+  const close = document.querySelector(".close");
+
+  //移除功能
+  container.addEventListener("click", (e) => {
+    console.log(e.target.nodeName);
+    if(e.target && e.target.nodeName == "SPAN"){
+      e.target.parentNode.remove();
+    }
+    
+    // goal = e.target.parentNode
+    // goal.remove();
+  })
+  
+  addBtn.addEventListener("click", (e) => {
+    // 幫 li 長 class name
+    const note = document.querySelector("li");
+    note.className = "todoItem";
+    container.appendChild(note);
+
+
+  })
+})
