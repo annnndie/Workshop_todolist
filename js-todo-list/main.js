d333bc89be6d60ab49a71563d5d22aeaa9ff561a
Erik <jjjay5223758@gmai.com>
2021-12-03 13:09:29 +0800

    finished t1

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,9 @@
-// TO DO
+// checked
+document.addEventListener("DOMContentLoaded", () => {
+    const todo = document.querySelector('ul');
+    todo.addEventListener('click', (e) => {
+        if (e.target.tagName ='li') {
+        e.target.classList.toggle('checked');
+        }
+    })
+})
